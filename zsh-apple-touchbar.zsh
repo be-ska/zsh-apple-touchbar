da3b90591679b382faae0a1ee8b96ce869b78f66
Luca Pescante <72148330+be-ska@users.noreply.github.com>
2024-03-29 13:57:17 +0800

    modify shortcuts

--- zsh-apple-touchbar.zsh
+++ zsh-apple-touchbar.zsh
@@ -9,41 +9,19 @@ function first_view() {
 
 	create_key 1 '🔌 usb' 'ls /dev | grep tty.usb' '-s'
 	create_key 2 '👨🏻‍💻 code' 'cd ~/code' '-s'
-	create_key 3 '🤖 ap' 'cd ~/code/ardupilot' '-s'
+	create_key 3 '🚁 ap' 'cd ~/code/ardupilot' '-s'
 	create_key 4 '🚜 ap-rov' 'cd ~/code/ardupilot-rover' '-s'
-	create_key 5 'second view' 'second_view'
-	create_key 6 'third view' 'third_view'
-}
-
-function second_view() {
-	remove_and_unbind_keys
-
-	set_state 'second'
-
-	create_key 1 '👈 back' 'first_view'
-	create_key 2 'current path' 'pwd' '-s'
-
-	set_state 'first'
-}
-
-function third_view() {
-	remove_and_unbind_keys
-
-	set_state 'third'
-
-	create_key 1 '👈 back' 'first_view'
-	create_key 2 'ls' 'ls -la' '-s'
+	create_key 5 '🤖 ap-sim' 'Tools/autotest/sim_vehicle.py -v Copter --console --map' '-s'
+	create_key 6 '✈️ proxy' 'cd ~/code/Lua-script-Align && mavproxy.py --master=/dev/tty.usbmodem14101 --baudrate=115200 --console' '-s'
+	create_key 7 '📂 path' 'pwd' '-s'
+	create_key 8 '🦚 VSC' 'code .' '-s'
 }
 
 zle -N first_view
-zle -N second_view
-zle -N third_view
 
 precmd_apple_touchbar() {
 	case $state in
 		first) first_view ;;
-		second) second_view ;;
-		third) third_view ;;
 	esac
 }
 
